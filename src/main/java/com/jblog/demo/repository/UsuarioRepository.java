1f20fd7f018bc79ebc6e98bf336cb92c3d0c109e
juan-saracco <juansaracco1212@gmail.com>
2023-10-01 20:20:54 -0300

    [ADD] produccion

--- src/main/java/com/jblog/demo/repository/UsuarioRepository.java
+++ src/main/java/com/jblog/demo/repository/UsuarioRepository.java
@@ -3,7 +3,6 @@ package com.jblog.demo.repository;
 import com.jblog.demo.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
